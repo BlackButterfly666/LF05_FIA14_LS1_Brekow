4794d4e72d9b7a65dd821e8c6705200b2570ddaa
Anne <35565463+BlackButterfly666@users.noreply.github.com>
2021-12-30 22:41:59 +0100

    A1.2 - Erweiterung "Hello World" durchführen

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
+		System.out.println("Guten Tag Anne. \nWillkommen in der Veranstaltung Strukturierte Programmierung.");
 
 
 	}
