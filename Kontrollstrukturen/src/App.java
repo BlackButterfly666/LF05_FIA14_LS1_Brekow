c6cb8230e4bf8e9e9e0e498239b73e08ea7dea65
Anne <35565463+BlackButterfly666@users.noreply.github.com>
2022-01-01 00:59:15 +0100

    A4.1: Einführung Auswahlstrukturen Aufgabe 6: Funktionslöser

--- Kontrollstrukturen/src/App.java
+++ Kontrollstrukturen/src/App.java
@@ -1,11 +1,11 @@
-
 public class App {
     // Operatoren, logische Ausdrücke, Auswahlanweisungen
     public static void main(String[] args) throws Exception {
-        //Aufgabe1.aufgabe1();
+        // Aufgabe1.aufgabe1();
         // Aufgabe2.aufgabe2();
         // Aufgabe3.aufgabe3();
         // Aufgabe4.aufgabe4();
-        Aufgabe5.aufgabe5();
+        // Aufgabe5.aufgabe5();
+        Aufgabe6.aufgabe6();
     }
 }
---
+++ Kontrollstrukturen/src/Aufgabe6.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Aufgabe6 {
+    public static void aufgabe6() {
+        Scanner tastatur = new Scanner(System.in);
+        System.out.println("Wie groß ist x?");
+        double x = tastatur.nextDouble();
+        double e = 2.718;
+
+        if (x <= 0) {
+            x = e * x;
+            System.out.printf("x = %.2f, exponentiell", x);
+        } 
+        else if (x > 0 && x <= 3) {
+            x = (x * x) + 1;
+            System.out.printf("x = %.2f, quadratisch", x);
+        } 
+        else {
+            x = 2 * x + 4;
+            System.out.printf("x = %.2f, linear", x);
+        }
+    }
+}
+
+//
+/*
+ * Eine Funktion y = f(x) ist in folgenden Bereichen definiert:
+ * x ≤ 0 | f(x) = ex | exponentiell
+ * 0 < x ≤ 3 | f(x) = x2 + 1 | quadratisch
+ * x > 3 | f(x) = 2  x + 4 | linear
+ * Man lese einen Wert für x ein und gebe den Funktionswert zusammen mit der
+ * Meldung aus, in welchem Bereich sich der Wert befindet (konstant, linear,
+ * ...).
+ * Für die Eulersche Zahl e verwenden sie eine Konstante mit dem Wert 2,718.
+ */
