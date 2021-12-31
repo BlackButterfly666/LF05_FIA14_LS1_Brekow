22b1b361c4dda796cb2f7bf348ba9f9ad3adf1e0
Anne <35565463+BlackButterfly666@users.noreply.github.com>
2021-12-31 01:27:12 +0100

    A2.5: Anzahl der Tickets hinzufügen
    Aufgabe 1-4

--- Fahrkartenautomat/src/Fahrkartenautomat.java
+++ Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -7,19 +7,27 @@ class Fahrkartenautomat
        Scanner tastatur = new Scanner(System.in);
       
        double zuZahlenderBetrag; 
+       int anzahlTickets; 
        double eingezahlterGesamtbetrag;
        double eingeworfeneMünze;
        double rückgabebetrag;
 
        System.out.print("Zu zahlender Betrag (EURO): ");
        zuZahlenderBetrag = tastatur.nextDouble();
+       System.out.print("Wieviele Tickets werden benötigt? ");
+       anzahlTickets = tastatur.nextInt();
+
+       // Berechnung Gesamtpreis der Tickets
+       // -----------
+       zuZahlenderBetrag *= anzahlTickets;
+       //System.out.printf("Zu zahlender Betrag (EURO): \n%.2f\n", zuZahlenderBetrag);
 
        // Geldeinwurf
        // -----------
        eingezahlterGesamtbetrag = 0.0;
        while(eingezahlterGesamtbetrag < zuZahlenderBetrag)
        {
-    	   System.out.printf("Noch zu zahlen: %.2f\n",zuZahlenderBetrag - eingezahlterGesamtbetrag);
+    	   System.out.printf("Noch zu zahlen: %.2f EURO\n",zuZahlenderBetrag - eingezahlterGesamtbetrag);
     	   System.out.print("Eingabe (mind. 5Ct, höchstens 2 Euro): ");
     	   eingeworfeneMünze = tastatur.nextDouble();
            eingezahlterGesamtbetrag += eingeworfeneMünze;
@@ -85,3 +93,30 @@ class Fahrkartenautomat
                           "Wir wünschen Ihnen eine gute Fahrt.");
     }
 }
+
+/*  A 2.5
+    Aufgabe 1: Notieren Sie die Variablen, die im Quelltext deklariert werden, und die Datentypen dieser Variablen.
+        Scanner tastatur = new Scanner(System.in);  => Scanner ist eine input-Abfrage
+        double zuZahlenderBetrag;                   => double (doppelt genaue Gleitkommazahl, Größe von 8 byte
+        double eingezahlterGesamtbetrag;            => double (doppelt genaue Gleitkommazahl, Größe von 8 byte
+        double eingeworfeneMünze;                   => double (doppelt genaue Gleitkommazahl, Größe von 8 byte
+        double rückgabebetrag;                      => double (doppelt genaue Gleitkommazahl, Größe von 8 byte
+   
+    Aufgabe 2: Welche Operationen werden mit den Variablen ausgeführt?
+        Scanner tastatur = new Scanner(System.in);  -
+        double zuZahlenderBetrag;                   wird gefüllt mit der Tastatureingabe vom Scanner, wird subtrahiert (arithmetische Operatoren), 
+                                                    Vergleich zu 'kleiner als' (relationale Operatoren)
+        double eingezahlterGesamtbetrag;            wird subtrahiert (arithmetische Operatoren), wird addiert (arithmetische Operatoren), 
+                                                    Vergleich zu 'kleiner als' (relationale Operatoren)
+        double eingeworfeneMünze;                   wird gefüllt mit der Tastatureingabe vom Scanner, wird addiert (arithmetische Operatoren)
+        double rückgabebetrag;                      Vergleich zu 'größer als' (relationale Operatoren)
+
+    Aufgabe 3: Ergänzen Sie das Programm so, dass nach Eingabe des Einzelpreises die Anzahl der Fahrkarten eingegeben werden kann:
+        Ticketpreis (Euro): 2.5
+        Anzahl der Tickets: 2
+        Noch zu zahlen: 5.00 Euro
+
+    Aufgabe 4: Anschließend testen Sie das Programm und - wenn alle Fehler korrigiert sind - übertragen Sie die aktuelle Version in Ihr Repository (Git: push).
+        commit, da push keinen Sinn macht, wenn noch 2 Aufgaben bearbeitet werden
+    
+*/
