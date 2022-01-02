7ca78370bc5e3da106e3604a0e43082072ffe610
Anne <35565463+BlackButterfly666@users.noreply.github.com>
2022-01-02 16:48:32 +0100

    A4.1: Fallunterscheidungen Aufgabe 1: Noten

---
+++ Kontrollstrukturen2/src/Aufgabe1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Aufgabe1 {
+    public static void aufgabe1() {
+        Scanner tastatur = new Scanner(System.in);
+
+        System.out.println("Bitte gib eine Zahl als Note ein. (Eingabe Zahlen: 1-6)");
+        int note = tastatur.nextInt();
+        boolean korrekteEingabe = false;
+
+        while (!korrekteEingabe) {
+            if (note > 0 && note <= 6) {
+                switch (note) {
+                    case 1:
+                        System.out.println("Sehr gut");
+                        korrekteEingabe = !korrekteEingabe;
+                        break;
+                    case 2:
+                        System.out.println("Gut");
+                        korrekteEingabe = !korrekteEingabe;
+                        break;
+                    case 3:
+                        System.out.println("Befriedigend");
+                        korrekteEingabe = !korrekteEingabe;
+                        break;
+                    case 4:
+                        System.out.println("Ausreichend");
+                        korrekteEingabe = !korrekteEingabe;
+                        break;
+                    case 5:
+                        System.out.println("Mangelhaft");
+                        korrekteEingabe = !korrekteEingabe;
+                        break;
+                    case 6:
+                        System.out.println("Ungenügend");
+                        korrekteEingabe = !korrekteEingabe;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                System.out.println("Falsche Eingabe. Bitte gib eine Zahl zwischen 1 und 6 ein.");
+                note = tastatur.nextInt();
+            }
+            tastatur.close();
+        }
+    }
+}
+
+// Aufgabe 1: Noten
+/*
+ * Erstellen Sie die Konsolenanwendung Noten. Das Programm Noten soll nach der
+ * Eingabe einer Ziffer die sprachliche Umschreibung ausgeben
+ * (1 = Sehr gut, 2 = Gut, 3 = Befriedigend, 4 = Ausreichend, 5 = Mangelhaft, 6
+ * = Ungenügend).
+ * Falls eine andere Ziffer eingegeben wird, soll ein entsprechender
+ * Fehlerhinweis ausgegeben werden.
+ */
