c91196a11f32cb8eff2dd36a8baa5aa6f4eb78ef
Anne <35565463+BlackButterfly666@users.noreply.github.com>
2022-01-18 11:39:31 +0100

    A5.2: Übung/Anwendung Array - Einfache Übungen zu Arrays Aufgabe 3

---
+++ Array1/src/Aufgabe3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Aufgabe3 {
+    static void aufgabe3() {
+        Scanner tastatur = new Scanner(System.in);
+        System.out.println("Gib mir ein Wort, was ich rückwärts wiedergebe.");
+        String palindrom[] = tastatur.nextLine().split("");
+
+        for (int i = palindrom.length -1; i >= 0; i--) {
+            System.out.print(palindrom[i]);
+        }
+    }
+}
+// Aufgabe 3
+/*
+ * Im Programm „Palindrom“ werden über die Tastatur 5 Zeichen eingelesen und in
+ * einem geeigneten Array gespeichert. Ist dies geschehen, wird der Arrayinhalt
+ * in umgekehrter Reihenfolge (also von hinten nach vorn) auf der Konsole
+ * ausgegeben.
+ */
